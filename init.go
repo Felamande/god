83e675bab894ae3924435fb9d034bfdc0903edfd
Felamande <elweinfancon@gmail.com>
2016-04-04 22:53:28 +0800

    fix default god.js

--- init.go
+++ init.go
@@ -85,11 +85,9 @@ func initfn(c *cli.Context) {
 const SEEK_END = 2
 const SEEK_CUR = 1
 
-const defaultjs = `
-
-//modules located in github.com/Felamande/jsvm/modules 
+const defaultjs = `//modules located in github.com/Felamande/jsvm/modules 
 //and github.com/Felamande/god/modules
-//you can write modules yourself if you're familiar witch otto.
+//you can write modules yourself if you're familiar with otto.
 god = require("god")
 log = require("log")
 path = require("path")
@@ -105,7 +103,7 @@ var binArgs = []
 var testArgs = [] //args for reloaded binaries 
 
 //will be call after god starts and before god watches changes.
-god.init(function() { go.reload(".", buildArgs, binArgs)})
+god.init(function() {console.log("hello");go.reload(".", buildArgs, binArgs)})
 
 // event 
 // event.rel, relative path of matched file or directory
@@ -114,6 +112,7 @@ god.init(function() { go.reload(".", buildArgs, binArgs)})
 // path seperator will be slash on windows.
 god.watch(["*_test.go", "**/*_test.go"], true,
     function(event) {
+        log.info("test",event.dir)
         go.test(event.dir, testArgs, function(err) { log.error(err) })
     }
 )
@@ -122,14 +121,16 @@ god.watch(["*_test.go", "**/*_test.go"], true,
 // * will match just ONE directory or as many chars as possible except slash .
 god.watch("**/*.go", false,
     function(event) {
+        log.info("install",event.dir)
         go.install(event.dir, installArgs, function(err) { log.error(err) })
     }
 )
 
-god.watch(["*.go"], false,
+god.watch(["*.go","**/*.go"], false,
     function(event) {
-        log.info("reload", event.rel, )
+        log.info("reload", event.dir)
         go.reload(".", buildArgs, binArgs, function(err) { if (err) { log.error(err) } })
     }
 )
+
 `
