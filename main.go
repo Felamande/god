7895a6e412e75674c31c52d5cc3f1e07feb3380b
Felamande <elweinfancon@gmail.com>
2016-04-17 19:24:15 +0800

    use a manager struct to manage subcommands

--- main.go
+++ main.go
@@ -5,8 +5,11 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 
-	"github.com/Felamande/jsvm"
+	"gopkg.in/fsnotify.v1"
+
+	"github.com/Felamande/god/lib/jsvm"
 
 	"github.com/codegangsta/cli"
 	"github.com/peterh/liner"
@@ -15,18 +18,36 @@ import (
 	"github.com/Felamande/god/modules/god"
 	"github.com/Felamande/god/modules/hotkey"
 	_ "github.com/Felamande/god/modules/log"
-	_ "github.com/Felamande/jsvm/module/os"
-	_ "github.com/Felamande/jsvm/module/path"
+	_ "github.com/Felamande/god/modules/os"
+	_ "github.com/Felamande/god/modules/path"
 )
 
-var version = "2.0.0-beta"
+var version = "2.0.0-beta2"
+
+//Cmder command manager
+type Cmder struct {
+	line      *liner.State
+	history   *os.File
+	w         *fsnotify.Watcher
+	stopWChan chan bool
+	watchm    *sync.Mutex
+	wd        string
+
+	global map[string]string
+}
+
+var cmder = &Cmder{
+	stopWChan: make(chan bool, 1),
+	watchm:    new(sync.Mutex),
+	global:    make(map[string]string),
+}
 
 func init() {
 	err := jsvm.Run("god.js")
 	if err != nil {
 		fmt.Println(err)
 	}
-
+	cmder.global["ps1"] = "(god) "
 }
 func main() {
 
@@ -35,30 +56,46 @@ func main() {
 	app.Name = filepath.Base(os.Args[0])
 	app.HelpName = app.Name
 	app.Usage = "An automation tool for Go project."
-	app.Action = enterAction
-	app.Commands = append(app.Commands, initCmd, watchCmd, reloadCmd, exitCmd)
+	app.Action = cmder.enterAction
+	app.Commands = append(app.Commands, initCmd, watchCmd, reloadCmd, exitCmd, historyCmd)
 
 	for name, CbFunc := range god.SubCmd {
 		app.Commands = append(app.Commands, newSubCmd(name, CbFunc))
+	}
+
+	err := hotkey.Bind("ctrl+d", func() { cmder.exit(nil) })
+	if err != nil {
+		fmt.Println(err)
+
+	}
+	err = hotkey.Bind("ctrl+alt+p", func() { cmder.unwatch(nil) })
+	if err != nil {
+		fmt.Println(err)
 	}
 
 	go hotkey.ApplyAll()
 	app.Run(os.Args)
 }
 
-func enterAction(c *cli.Context) {
+func (c *Cmder) enterAction(ctx *cli.Context) {
+
 	god.Init.Call()
 	line := liner.NewLiner()
 	defer line.Close()
 	line.SetCtrlCAborts(false)
+	c.line = line
 
 	historyFile, err := os.OpenFile(filepath.Join(os.TempDir(), ".god_hostory"), os.O_CREATE|os.O_RDWR, 0777)
-	if err == nil {
-		line.ReadHistory(historyFile)
+
+	if err != nil {
+		fmt.Println(err)
+
 	}
+	c.history = historyFile
+	line.ReadHistory(historyFile)
 
 	for {
-		argStr, err := line.Prompt(`(god) `)
+		argStr, err := line.Prompt(c.global["ps1"])
 		argStr = strings.TrimSpace(argStr)
 		if len(argStr) == 0 {
 			continue
@@ -70,6 +107,7 @@ func enterAction(c *cli.Context) {
 			fmt.Println(err)
 			continue
 		}
+
 		splits := strings.Split(argStr, " ")
 		var args = append([]string{}, "god")
 		for _, s := range splits {
@@ -80,11 +118,11 @@ func enterAction(c *cli.Context) {
 		if len(args) == 0 {
 			continue
 		}
-		if c.App.Command(args[1]) == nil {
+		if ctx.App.Command(args[1]) == nil {
 			continue
 		}
 
-		c.App.Run(args)
+		ctx.App.Run(args)
 
 	}
 
